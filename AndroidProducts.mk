98bb076d8f95a94a32def144db0d5a1895dd6c36
GIoKas93 <giorgio_castelluccilibero.it>
2021-03-01 09:23:30 +0000

    Aaaand... DOT!!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ssos_raphael.mk
+    $(LOCAL_DIR)/dot_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    ssos_raphael-user \
-    ssos_raphael-userdebug \
-    ssos_raphael-eng
+    dot_raphael-user \
+    dot_raphael-userdebug \
+    dot_raphael-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf hardware/qcom-caf/sm8150/display
+rm -rf vendor/qcom/opensource/vibrator
+rm -rf vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+rm -rf packages/apps/Bluetooth
+rm -rf system/bt
+rm -rf vendor/qcom/opensource/commonsys/system/bt
+rm -rf external/sqlite
+git clone https://github.com/ShapeShiftOS/android_vendor_qcom_opensource_system_bt -b android_11 vendor/qcom/opensource/commonsys/system/bt
+git clone https://github.com/ShapeShiftOS/android_system_bt -b android_11 system/bt
+git clone https://github.com/ShapeShiftOS/android_packages_apps_Bluetooth -b android_11 packages/apps/Bluetooth
+git clone https://github.com/ShapeShiftOS/android_vendor_qcom_opensource_packages_apps_Bluetooth -b android_11 vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+git clone https://github.com/ShapeShiftOS/android_vendor_qcom_opensource_vibrator -b android_11 vendor/qcom/opensource/vibrator
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone https://github.com/yaap/hardware_qcom-caf_sm8150_audio -b eleven hardware/qcom-caf/sm8150/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/syberia-project/platform_external_sqlite -b 11.0 external/sqlite
